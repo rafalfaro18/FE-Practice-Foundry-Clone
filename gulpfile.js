d17945a1d2b70d8ed3c8b9d43d5499dbf886a22e
Rafael Alfaro <rafaela@thehangar.cr>
2018-01-24 21:59:19 -0600

    sass task default task and watch task

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,15 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 
-function defaultTask() {
-  console.log('Hello World');
-}
+const sassfiles = 'src/scss/**/*.scss';
 
 function compilesass() {
-  return gulp.src('src/scss/**/*.scss')
+  return gulp.src(sassfiles)
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('dist/css'));
 }
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', compilesass);
-
-gulp.task('default', defaultTask);
+gulp.task('watch', gulp.watch(sassfiles).on('change'), sass);
+gulp.task('default', sass);
