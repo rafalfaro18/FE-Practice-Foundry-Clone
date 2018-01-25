242d07fcdbe0d2383b29e6690a272791dd8952de
Rafael Alfaro <rafaela@thehangar.cr>
2018-01-24 21:51:33 -0600

    includes gulp sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,17 @@
 const gulp = require('gulp');
+const sass = require('gulp-sass');
 
 function defaultTask() {
   console.log('Hello World');
 }
+
+function compilesass() {
+  return gulp.src('src/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('dist/css'));
+}
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', compilesass);
 
 gulp.task('default', defaultTask);
